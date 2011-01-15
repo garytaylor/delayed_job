aa1dea0abcbb2d22f17330625404a6088d30b466
Gary Taylor <garywork@Gary-Taylors-MacBook-Pro.local>
2011-01-15 17:14:36 +0000

    Delayed::Job is not setup if the table does not exist - allows migrations to continue

--- lib/delayed/railtie.rb
+++ lib/delayed/railtie.rb
@@ -4,7 +4,11 @@ require 'rails'
 module Delayed
   class Railtie < Rails::Railtie
     initializer :after_initialize do
-      Delayed::Worker.guess_backend
+      if ActiveRecord::Base.connection.tables.include?('delayed_jobs')
+        Delayed::Worker.guess_backend
+      else
+        puts "Warning: Delayed::Job not loaded due to missing table.  If you are not migrating, please add the delayed job migration to your application"
+      end
 
       ActiveSupport.on_load(:action_mailer) do
         ActionMailer::Base.send(:extend, Delayed::DelayMail)
